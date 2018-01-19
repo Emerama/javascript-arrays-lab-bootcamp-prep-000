f61b746eb7657fd4674626a4cb74c9d334243c41
Emily Ranson <ransonme@tcd.ie>
2018-01-19 22:31:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(name) {
-  kittens.unshift("Ralph")
+function destructivelyAppendKitten(kittens) {
+  kittens.push("Ralph")
   return kittens
 }
